e6fc9aa3847418b430ca3b41ece27a3bb701ebf5
Henry Apler <henry.apler@gmail.com>
2023-09-27 15:50:13 +0200

    Fixed and working!

--- calls_per_hour_per_day.py
+++ calls_per_hour_per_day.py
@@ -10,33 +10,26 @@ df = pd.DataFrame(data)
 
 bar_df = df.iloc[0, [5, 6, 7, 8, 9, 10, 11, 12]]
 n_df = pd.DataFrame(bar_df)
-print(n_df)
+n_df.reset_index(inplace=True)
+n_df.rename(
+    columns={"index": "Hours", 0: "No of Calls"},
+    inplace=True,
+)
+app = Dash(__name__)
 
-# hours_column = bar_df.iloc[:,1]
-# no_calls_column = bar_df.iloc[:,2]
-# mod_df = pd.DataFrame({'Hours': hours_column, 'No of Calls': no_calls_column})
-
-# # Skapa en lista med kolumnnamnen
-# hours_column = bar_df.columns.tolist()
-
-# # Skapa en DataFrame med kolumnnamnen som "Hours" och data från bar_df som "No of Calls"
-# mod_df = pd.DataFrame({'Hours': hours_column, 'No of Calls': bar_df.loc[1,].values.flatten()})
-
-# app = Dash(__name__)
-
-# fig = px.bar(mod_df, x='Hours', y='No of Calls', title='Todays Calls',color='Hours')
+fig = px.bar(n_df, x='Hours', y='No of Calls', title='Todays Calls',color='Hours')
 
 
 
-# app.layout= html.Div([
-#     html.Div([
-#             html.H1('Todays Calls'),
-#             dcc.Graph(
-#                 id='bar-plot',
-#                 figure=fig,
-#                 style={'width':'45%', 'height':'50%'})
-#                 ])])
+app.layout= html.Div([
+    html.Div([
+            html.H1('Todays Calls'),
+            dcc.Graph(
+                id='bar-plot',
+                figure=fig,
+                style={'width':'45%', 'height':'50%'})
+                ])])
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
