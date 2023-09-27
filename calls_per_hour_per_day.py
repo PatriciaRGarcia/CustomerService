9ada67abade1f6062a30293d70e28aefe0726407
Henry Apler <henry.apler@gmail.com>
2023-09-27 15:35:01 +0200

    trying to figure it out

--- calls_per_hour_per_day.py
+++ calls_per_hour_per_day.py
@@ -5,32 +5,38 @@ import matplotlib.pyplot as plt
 
 
 
-data = pd.read_csv('data/test_data_original.csv')
+data = pd.read_csv('data/testData_month_orig.csv')
 df = pd.DataFrame(data)
 
-bar_df = df.iloc[:, [6, 7, 8, 9, 10, 11, 12, 13]]
+bar_df = df.iloc[0, [5, 6, 7, 8, 9, 10, 11, 12]]
+n_df = pd.DataFrame(bar_df)
+print(n_df)
 
-# Skapa en lista med kolumnnamnen
-hours_column = bar_df.columns.tolist()
+# hours_column = bar_df.iloc[:,1]
+# no_calls_column = bar_df.iloc[:,2]
+# mod_df = pd.DataFrame({'Hours': hours_column, 'No of Calls': no_calls_column})
 
-# Skapa en DataFrame med kolumnnamnen som "Hours" och data från bar_df som "No of Calls"
-mod_df = pd.DataFrame({'Hours': hours_column, 'No of Calls': bar_df.values.flatten()})
+# # Skapa en lista med kolumnnamnen
+# hours_column = bar_df.columns.tolist()
 
-app = Dash(__name__)
+# # Skapa en DataFrame med kolumnnamnen som "Hours" och data från bar_df som "No of Calls"
+# mod_df = pd.DataFrame({'Hours': hours_column, 'No of Calls': bar_df.loc[1,].values.flatten()})
 
-fig = px.bar(mod_df, x='Hours', y='No of Calls', title='Todays Calls',color='Hours')
+# app = Dash(__name__)
 
-
-
-app.layout= html.Div([
-    html.Div([
-            html.H1('Todays Calls'),
-            dcc.Graph(
-                id='bar-plot',
-                figure=fig,
-                style={'width':'45%', 'height':'50%'})
-                ])])
+# fig = px.bar(mod_df, x='Hours', y='No of Calls', title='Todays Calls',color='Hours')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+# app.layout= html.Div([
+#     html.Div([
+#             html.H1('Todays Calls'),
+#             dcc.Graph(
+#                 id='bar-plot',
+#                 figure=fig,
+#                 style={'width':'45%', 'height':'50%'})
+#                 ])])
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
