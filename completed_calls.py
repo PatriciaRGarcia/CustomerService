b9b8ff1c8717f5f5e6c3ca825b8233b0b13a2b85
Yatwan Hui <peter.bjorkholm@molk.com>
2023-09-27 16:35:12 +0200

    First commit of completed_calls at the end of the day

---
+++ completed_calls.py
@@ -0,0 +1,12 @@
+import plotly.express as px
+import pandas as pd
+
+
+df = pd.read_csv("CustomerService/data/testData_month_orig.csv")
+
+row_0 = df.iloc[0]
+
+total_calls = row_0['calls_at_9'] + row_0['calls_at_10'] + row_0['calls_at_11'] + row_0['calls_at_12'] + row_0['calls_at_13'] + row_0['calls_at_14']+ row_0['calls_at_15'] + row_0['calls_at_16']
+
+print(f"Completed calls so far: {total_calls}")
+
